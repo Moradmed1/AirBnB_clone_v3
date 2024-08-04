08f9fb68caf80de1ada561352433bcbc815d60bd
Moradmed1 <Moradmed1@gmail.com>
2024-08-04 13:55:31 -0700

    My first commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,3 +29,8 @@ if __name__ == "__main__":
     API_HOST = os.getenv('HBNB_API_HOST') or '0.0.0.0'
     API_PORT = os.getenv('HBNB_API_PORT') or 5000
     app.run(host=API_HOST, port=API_PORT, threaded=True)
+
+if __name__ == "__main__":
+    API_HOST = os.getenv('HBNB_API_HOST') or '0.0.0.0'
+    API_PORT = os.getenv('HBNB_API_PORT') or 5000
+    app.run(host=API_HOST, port=API_PORT, threaded=True)
