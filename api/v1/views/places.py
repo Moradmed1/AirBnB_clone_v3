3f1344e5e42765c1f4b66442c66b3eb8b6f3a8b6
Moradmed1 <125490749+Moradmed1@users.noreply.github.com>
2024-08-04 22:11:09 +0000

    Update places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python3     # Shebang line to specify the interpreter
 """places view"""
+# Import necessary modules and classes from Flask and the project's packages
 from flask import abort, request, jsonify
-
+ # Flask modules for handling requests and responses
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
@@ -10,7 +11,7 @@ from models.user import User
 from models.state import State
 from models.amenity import Amenity
 
-
+# Define a route to get all places of a specific city
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
 def places_of_city(city_id):
     """display all places of a city"""
