84d07bcfd58b2d61240f533073a455b4ce1b3f6d
Moradmed1 <Moradmed1@gmail.com>
2024-08-04 16:35:05 -0700

    My first commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 """Entry point of the api"""
 import os
 from flask_cors import CORS
-
-# Import flask model
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
@@ -32,7 +29,3 @@ if __name__ == "__main__":
     API_HOST = os.getenv('HBNB_API_HOST') or '0.0.0.0'
     API_PORT = os.getenv('HBNB_API_PORT') or 5000
     app.run(host=API_HOST, port=API_PORT, threaded=True)
-
-#    if __name__ == "__main__":
-#    API_HOST = os.getenv('HBNB_API_HOST') or '0.0.0.0'
-#    API_PORT = os.getenv('HBNB_API_PORT') or 5000
